5f787d2f32b3ae82fbdf5b48d554ec301305c1e6
Leandro <leandro.martins1912@gmail.com>
2021-10-07 15:27:35 -0300

    Added BeachesController.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,43 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
+import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
-import { Application } from 'express';
-import * as database from '@src/database'
+import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
+
 export class SetupServer extends Server {
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
   constructor(private port = 3000) {
     super();
   }
 
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
   public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
-    await this.databaseSetup()
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.setupControllers();
   }
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
   }
 
   private async databaseSetup(): Promise<void> {
@@ -30,9 +46,5 @@ export class SetupServer extends Server {
 
   public async close(): Promise<void> {
     await database.close();
-  }
-
-  public getApp(): Application {
-    return this.app;
   }
 }
