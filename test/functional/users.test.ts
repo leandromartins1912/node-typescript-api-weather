aed0e4d5a239c087b6c4946b32ec669a45d950fd
Leandro <leandro.martins1912@gmail.com>
2021-10-20 14:58:05 -0300

    Added Users test

---
+++ test/functional/users.test.ts
@@ -0,0 +1,21 @@
+import { User } from "@src/models/user"
+
+describe('users functional tests', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+    })
+    describe('When creating a new user', () => {
+        it('should successfully create a new user', async() => {
+            const newUser = {
+                name: 'Jon Doe',
+                email: 'jon@mail.com',
+                password: '1234',
+            }
+
+            const response = await global.testRequest.post('/users').send(newUser);
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(expect.objectContaining(newUser));
+
+        })
+    })
+})
