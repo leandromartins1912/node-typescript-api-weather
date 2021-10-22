8c886b42c6cb6b79304b23cca74fc9356c2debd9
Leandro <leandro.martins1912@gmail.com>
2021-10-22 14:42:50 -0300

    Added logic for compare pass cripted before save

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document } from 'mongoose';
-
+import AuthService from '@src/services/auth';
 export interface User {
     _id?: string;
     name: string;
@@ -43,6 +43,20 @@ schema.path('email').validate(
     'already exists in the database.',
     CUSTOM_VALIDATION.DUPLICATED
 );
+
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+    if (!this.password || !this.isModified('password')) {
+        return;
+    }
+
+    try {
+        const hashedPassword = await AuthService.hashPassword(this.password);
+        this.password = hashedPassword;
+    } catch (error) {
+        console.error(`Error hashing the password for the user ${this.name}`);
+    }
+})
 
 interface UserModel extends Omit<User, '_id'>, Document { }
 export const User = mongoose.model<UserModel>('User', schema);
