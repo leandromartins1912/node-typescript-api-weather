075188ea6a3cabd8eaf6254ab847acd781a986ae
Leandro <leandro.martins1912@gmail.com>
2021-10-25 15:23:34 -0300

    Added User in beach

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 
 export enum BeachPosition {
   S = 'S',
@@ -13,6 +13,7 @@ export interface Beach {
   position: BeachPosition;
   lat: number;
   lng: number;
+  user: string;
 }
 
 const schema = new mongoose.Schema(
@@ -21,6 +22,7 @@ const schema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', require: true },
   },
   {
     toJSON: {
