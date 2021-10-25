5e7704d3a7fcd66e13aa9f0f566883f6c3965e33
Leandro <leandro.martins1912@gmail.com>
2021-10-25 15:24:37 -0300

    Added generateToken and decodeToken.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import config from 'config';
+import { User } from '@src/models/user';
 
+export interface DecodedUser extends Omit<User, '_id'> {
+    id: string;
+}
 export default class AuthService {
 
     public static async hashPassword(password: string, salt = 10): Promise<string> {
@@ -12,9 +16,13 @@ export default class AuthService {
         return await bcrypt.compare(password, hashedPassword);
     }
 
-    public static generateToken(payload: object): string{
+    public static generateToken(payload: object): string {
         return jwt.sign(payload, config.get('App.auth.key'), {
             expiresIn: config.get('App.auth.tokenExpireIn')
         })
+    }
+
+    public static decodeToken(token: string): DecodedUser {
+        return jwt.verify(token, config.get('App.auth.key')) as DecodedUser;
     }
 }
